dac236d854b0addc4f5ed9ba89a7128a375f9022
bransyahtan <bransyahhhh@gmail.com>
2024-02-02 00:50:14 +0700

    update

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+// const MONGODB_URI = process.env.MONGODB_URI;
+
+const MONGODB_URI =
+  "mongodb+srv://bransyatan:123123123@cluster0.zkj66ji.mongodb.net/?retryWrites=true&w=majority";
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
